0c19d79677e3dd63688196c13e57fd5cba11be07
Amr Abdou <amrabdou@webleapgarage.com>
2023-04-24 07:22:54 +0700

    Implemented with express server

--- helper.js
+++ helper.js
@@ -12,19 +12,9 @@ class authHelper{
 
     }
 
-    // Generate Random String for the Application Id
-    static async generateRandomString(length) {
-        var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-';
-        var charLength = chars.length;
-        var result = '';
-        for ( var i = 0; i < length; i++ ) {
-          result += chars.charAt(Math.floor(Math.random() * charLength));
-        }
-        console.log("kooo" + result);
-        return result;
-    }
-
-    // Get Auth Token
+    /*
+    * Get Auth Token
+    */
     static async getAuthToken(){
 
         try{
---
+++ routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const applicationController = require('./applicationController')
+
+const app = express();
+
+// Set Render Engine to EJS
+app.use('/public', express.static('public'));
+app.set('view engine', 'ejs');
+
+// Use Body-parser to handle JSON responses
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+
+// GET: Home Route
+router.get('/', async (req, res) => {
+    res.render('newMerchantApplication');
+});
+
+// POST: Create Application
+router.post('/create-application', async (req, res, next) => {
+    applicationController.createApplication(req, res, next);
+});
+
+// POST: Send Application to Merchant
+router.post('/send-application', async (req, res, next) => {
+    applicationController.sendApplication(req, res, next);
+});
+
+// POST: Submit Application
+router.post("/submit-application", (req, res, next) => {
+  applicationController.submitApplication(req, res, next);
+});
+
+module.exports = router;
